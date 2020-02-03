2a45b159c20b10769501b06ef38d3ced8539604d
rutaks <samrutakayile@awesomity.rw>
2020-02-03 13:57:59 +0200

    Added Routes & Mock Data initiation

--- server/app.js
+++ server/app.js
@@ -9,8 +9,9 @@ import logger from "morgan";
 import session from "express-session";
 import path from "path";
 import flashMessages from "connect-flash";
-import ErrorHandler from "./helpers/error-handler";
-
+import routes from "./routes/index.route";
+import AuthMocks from "./mocks/auth-mocks";
+import { debug } from "winston";
 env.env();
 
 /** Variable Definitions */
@@ -55,16 +56,16 @@ app.get("/intro", (req, res) => res.render("server/index"));
 
 /**
  * Route serving API Routes
- * @name /api/v1
+ * @name /
  * @function
  * @param {string} path - Express path
  */
-// app.use("/api/v1", api);
+app.use("/", routes);
 
 /** Error Handling */
 const handleError = (err, res) => {
   const { statusCode, message } = err;
-  return res.render("auth/message-window", {
+  return res.render("server/message-window", {
     title: statusCode,
     heading: `Oh No, ${statusCode}. Something Has Happened`,
     message: message
@@ -79,6 +80,9 @@ app.use((err, req, res, next) => {
 const runServer = async () => {
   try {
     await connectDB();
+    const check = AuthMocks.setupAdmin();
+    const message = check ? "Mocks Are Set" : "Mocks Are Not Set";
+    console.debug("INFO:", message);
     app.listen(port, () =>
       console.debug("INFO:", `RwandaTradesIn is now running on Port: ${port}`)
     );
