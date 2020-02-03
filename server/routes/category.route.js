6463f17b5a2263ee81885a4fff21edd0c0e63a3f
rutaks <samrutakayile@awesomity.rw>
2020-02-03 17:20:38 +0200

    Added Category Route

---
+++ server/routes/category.route.js
@@ -0,0 +1,13 @@
+import Router from "express";
+import categoryController from "../controllers/category.controller";
+
+const router = Router();
+
+router.get("/", categoryController.getAllCategories);
+router.get("/new", categoryController.getAddCategoryPage);
+router.post("/new", categoryController.addCategory);
+router.get("/modify/:categoryId", categoryController.getModifyCategoryPage);
+router.post("/modify/:categoryId", categoryController.modifyCategory);
+router.get("/remove/:categoryId", categoryController.removeCategory);
+
+export default router;
