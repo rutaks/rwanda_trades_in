fac713520504d52739a0427f5ea8745c47192512
rutaks <samrutakayile@awesomity.rw>
2020-02-04 22:03:07 +0200

    Added Product Model

---
+++ server/models/product.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  currency: {
+    type: String,
+    required: true,
+    enum: ["USD", "UAE", "RWF"]
+  },
+  description: {
+    type: String
+  },
+  mainPicture: {
+    type: String,
+    required: true
+  },
+  secondPicture: {
+    type: String
+  },
+  thirdPicture: {
+    type: String
+  },
+  fourthPicture: {
+    type: String
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    required: true
+  },
+  createOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model("Product", productSchema);
