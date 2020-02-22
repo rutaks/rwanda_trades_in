62deaeea11529cd3bba46da157fda69e1de2bf9e
rutaks <rutaksam@gmail.com>
2020-02-22 04:47:48 +0200

    Added Product Route

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -2,6 +2,7 @@ import Router from "express";
 import authRoute from "./auth.route";
 import adminRoute from "./admin.route";
 import categoryRoute from "./category.route";
+import productRoute from "./product.route";
 import isAuth from "../middlewares/isAuth";
 
 const router = Router();
@@ -9,5 +10,6 @@ const router = Router();
 router.use("/auth", authRoute);
 router.use("/admin", isAuth, adminRoute);
 router.use("/admin/categories", isAuth, categoryRoute);
+router.use("/admin/products", isAuth, productRoute);
 
 export default router;
